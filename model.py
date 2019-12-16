fe4c21f5bb535487c7c00d73df1b7c23bbd6496e
Lye Yongkang <kangguangli@hotmail.com>
2019-12-16 21:44:59 +0800

    add comment

--- model.py
+++ model.py
@@ -17,7 +17,15 @@ def split_x_y(df : pd.DataFrame) :
 def one_class_classifier():
 
     df = preprocess.basic_data()
+    # 此时df内的数据包含了csv的所有列，具体使用了那些csv文件见函数内部，如果想使用更多数据可以在此函数基础上修改
+    # 这里的df“好像”可以直接喂给一些对数据格式要求不严格的分类器，比如lightgbm，如果我没记错的话，也可能我记错了
+
     df = preprocess.basic_process(df)
+    # 在此函数中对特征进行了一些裁剪，细节见函数内部，代码比较简单，不详细介绍了
+    # 这列的df可以直接喂给分类器
+    # 如果想对特征做更精细的处理，可以在此函数基础上进行修改
+
+    # 训练分类器，分别计算正负例的精度和召回率
 
     sclaer = MinMaxScaler()
 
@@ -27,13 +35,11 @@ def one_class_classifier():
     test_postive = postive.sample(frac = 0.2, random_state = 0, axis = 0)
     train_postive = postive[~postive.index.isin(test_postive.index)]
 
-    # test_negtive = negtive.sample(frac = 0.2, random_state = 0, axis = 0)
-    # train_negtive = negtive[~negtive.index.isin(test_negtive.index)]
-    # unlabeled = train_postive.sample(frac = 0.2, random_state = 0, axis = 0)
-    # test = pd.concat([test_postive, negtive], ignore_index = True)
-
     train_x, _ = split_x_y(train_postive)
-    clf = OneClassSVM(kernel = 'rbf', gamma = 'auto', verbose = True, max_iter = 10000)
+    # OneClassSVM用来做离群检验，可以检测异常数据，适用于只知道正例的情况，是一种无监督方法
+    # 下面的参数中相对比较重要的是nu，代表输入数据中异常数据比例的上界
+    # 此时输入的全为正例，因此将其设为一个比较小的值
+    clf = OneClassSVM(kernel = 'rbf', gamma = 'auto', verbose = True, max_iter = -1, random_state = 0, nu = 1e-6)
     clf = clf.fit(sclaer.fit_transform(train_x))
     
     x, y = split_x_y(test_postive)
